a50d1f235575e071e687a8285181d9d78cc42236
Efrain Colmenares <efraxpc@gmail.com>
2019-11-16 06:59:44 -0500

    fix

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -61,7 +61,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
 </head>
-<body class="app">
+<body class="app app w-100 p-0">
 <!-- @TOC -->
 <!-- =================================================== -->
 <!--
