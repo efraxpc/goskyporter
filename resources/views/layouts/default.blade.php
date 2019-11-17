632795126873d6f687cae1da508345295ed215b1
Efrain Colmenares <efraxpc@gmail.com>
2019-11-17 04:45:02 -0500

    t

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -176,10 +176,10 @@
     <div class="page-container">
         <!-- ### $Topbar ### -->
         <div class="row">
-            <div class="col-xs-2 col-sm-2 col-md-5 col-lg-5" >
+            <div class="col-4" >
                 <a class="btn btn-success d-sm-none" role="button" href="{{ route('customer_create_home') }}"><i class="fas fa-plus"></i> Query</a>
             </div>
-            <div class="col-xs-2 col-sm-2 col-md-5 col-lg-5" >
+            <div class="col-4" >
                 <ul class="nav justify-content mobile_menu d-sm-none">
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Manage CRM</a>
@@ -195,7 +195,7 @@
                     </li>
                 </ul>
             </div>
-            <div class="col-xs-4 col-sm-4 col-md-2 col-lg-2">
+            <div class="col-xs-4 col-sm-4 col-md-2 ">
                 <ul class="nav justify-content-end" >
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Profile</a>
