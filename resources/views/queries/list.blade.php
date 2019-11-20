7870dcd3e67631eab3a5d3adc5ab059c8ecbea3c
Efrain Colmenares <efraxpc@gmail.com>
2019-11-20 04:29:50 -0500

    4

--- resources/views/queries/list.blade.php
+++ resources/views/queries/list.blade.php
@@ -69,13 +69,17 @@
                     "targets": 4,
                     "render": function (data, type, row, meta) {
 
-                        var str = '<div class="text-center"><ul style="list-style: none;">'
+                        var str = 'No remarks'
+                        if(row.remarks.length >=1 )
+                        {
+                            str = '<div class="text-center"><ul style="list-style: none;">'
 
-                        row.remarks.forEach(function(slide) {
-                            str += '<li>'+ slide + '</li>';
-                        });
+                            row.remarks.forEach(function(slide) {
+                                str += '<li>'+ slide + '</li>';
+                            });
 
-                        str += '</ul></div>';
+                            str += '</ul></div>';
+                        }
 
                         return str
                     }
