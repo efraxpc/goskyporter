d886b6e9c32bb1a44f846bc94cc75598f4cd90b9
efraxpc <efraxpc@gmail.com>
2019-11-23 06:10:33 -0800

    r

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -221,12 +221,14 @@
                 <ul class="nav justify-content mobile_menu d-sm-none">
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button"
-                           aria-haspopup="true" aria-expanded="false">Query</a>
+                           aria-haspopup="true" aria-expanded="false">Options</a>
                         <div class="dropdown-menu">
+                            <a class='dropdown-item' href="{{ route('home') }}"><i class="fas fa-columns"></i>
+                                New Query</a>
                             <a class='dropdown-item' href="{{ route('query_create_home') }}"><i class="fas fa-plus"></i>
-                                New</a>
+                                New Query</a>
                             <a class='dropdown-item' href="{{ route('queries') }}"><i
-                                    class="fas fa-list"></i> List</a>
+                                    class="fas fa-list"></i> List Queries</a>
                         </div>
                     </li>
             </div>
